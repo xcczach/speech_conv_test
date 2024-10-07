3d3a67ea68bd3f2bf9f5e150013cfaabc8132b64
Zhanxun Liu <xcc_zach@sjtu.edu.cn>
2024-10-07 17:22:56 +0800

    add tts

---
+++ models/tts.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from transformers import AutoProcessor, AutoModel
+
+class TTSModel(ABC):
+    @abstractmethod
+    def infer(self,texts: str|list[str]) -> tuple[np.ndarray|list[np.ndarray], int|float]:
+        pass
+
+class Bark(TTSModel):
+    def __init__(self, model_path: str):
+        self.processor = AutoProcessor.from_pretrained(model_path)
+        self.model = AutoModel.from_pretrained(model_path)
+
+    def infer(self,texts: str|list[str]) -> tuple[np.ndarray|list[np.ndarray], int|float]:
+        batch_op = True
+        if isinstance(texts, str):
+            texts = [texts]
+            batch_op = False
+        result_speechs = []
+        for text in texts:
+            inputs = self.processor(
+                text=[text],
+                return_tensors="pt",
+            )
+            speech_values = self.model.generate(**inputs, do_sample=True)
+            result_speechs.append(speech_values.cpu().numpy().squeeze())
+        sampling_rate = self.model.generation_config.sample_rate
+        if batch_op:
+            return result_speechs, sampling_rate
+        return result_speechs[0], sampling_rate 
